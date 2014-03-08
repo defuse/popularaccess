caa85780207f9fe1269055566993206132ada14e
Taylor Hornby <havoc@defuse.ca>
2014-03-08 10:38:14 -0700

    Add Piwik tracking code.

--- web/index.php
+++ web/index.php
@@ -51,5 +51,25 @@ header('Content-Type: text/html; charset=utf-8');
         URLParse::IncludePageContents();
     ?>
 
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://defuse.ca/piwik/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "3"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+
+<!-- Piwik Image Tracker -->
+<img src="https://defuse.ca/piwik/piwik.php?idsite=3&amp;rec=1" style="border:0" alt="" />
+<!-- End Piwik -->
+
 </body>
 </html>
